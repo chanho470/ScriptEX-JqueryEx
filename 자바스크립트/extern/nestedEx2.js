d5e227460a67f0d765e7e830aabbeed09af23040
chanho470 <chanho470@naver.com>
2021-12-30 18:20:11 +0900

    공부하자!!

---
+++ 자바스크립트/extern/nestedEx2.js
@@ -0,0 +1,3 @@
+document.write("nestedEx2.js 는ex1 에포함");
+document.write("<div style='color:blue;font-size:20px;'>외부 자바스크립트 파일</div>");
+document.write("<script src='extern/nestedEx3.js'></script>");
---
+++ 자바스크립트/extern/nestedEx3.js
@@ -0,0 +1,3 @@
+document.write("nestedEx3.js는ex2 에포함");
+document.write("<div style='color:green;font-size:16px;'>외부 자바스크립트 파일</div>");
+alert("nested script file");
